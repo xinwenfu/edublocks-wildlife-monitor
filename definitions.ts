0b2d871ff9cbdbb87f21e4de5b8d5406a0dca285
Xinwen Fu <69218457+xinwenfu@users.noreply.github.com>
2026-09-05 16:26:30 -0400

    Add files via upload

--- definitions.ts
+++ definitions.ts
@@ -1,83 +1,61 @@
 const wildlifeColor = "#4f7d55";
 
-Blockly.Blocks["wildlife_imports"] = {
-    init: function() {
-        this.appendDummyInput()
-            .appendField("import wildlife monitor libraries");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(wildlifeColor);
-    },
-};
+function statementBlock(label) {
+    return {
+        init: function() {
+            this.appendDummyInput().appendField(label);
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(wildlifeColor);
+        }
+    };
+}
+
+Blockly.Blocks["import_time"] = statementBlock("import time");
+Blockly.Blocks["import_subprocess"] = statementBlock("import subprocess");
+Blockly.Blocks["import_datetime"] = statementBlock("from datetime import datetime");
+Blockly.Blocks["import_motion_sensor"] = statementBlock("from gpiozero import MotionSensor");
+Blockly.Blocks["import_board"] = statementBlock("import board");
+Blockly.Blocks["import_adafruit_dht"] = statementBlock("import adafruit_dht");
 
 Blockly.Blocks["pir_setup"] = {
     init: function() {
-        this.appendDummyInput()
-            .appendField("set up PIR motion sensor on GPIO")
+        this.appendDummyInput().appendField("set up PIR motion sensor on GPIO")
             .appendField(new Blockly.FieldNumber(17, 0, 27, 1), "pin");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null); this.setNextStatement(true, null);
         this.setColour(wildlifeColor);
-    },
+    }
 };
+Blockly.Blocks["pir_wait_for_motion"] = statementBlock("wait for motion");
+Blockly.Blocks["pir_wait_for_no_motion"] = statementBlock("wait until motion stops");
 
 Blockly.Blocks["dht22_setup"] = {
     init: function() {
-        this.appendDummyInput()
-            .appendField("set up DHT22 sensor on GPIO")
+        this.appendDummyInput().appendField("set up DHT22 sensor on GPIO")
             .appendField(new Blockly.FieldNumber(4, 0, 27, 1), "pin");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null); this.setNextStatement(true, null);
         this.setColour(wildlifeColor);
-    },
+    }
 };
+Blockly.Blocks["dht22_read_safe"] = statementBlock("read temperature & humidity");
+Blockly.Blocks["camera_take_photo"] = statementBlock("take Raspberry Pi camera photo");
 
-Blockly.Blocks["pir_wait_for_motion"] = {
+Blockly.Blocks["microphone_record"] = {
     init: function() {
-        this.appendDummyInput()
-            .appendField("wait for motion");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
+        this.appendDummyInput().appendField("record sound");
+        this.appendDummyInput().appendField("device")
+            .appendField(new Blockly.FieldTextInput("plughw:1,0"), "device");
+        this.appendDummyInput().appendField("duration")
+            .appendField(new Blockly.FieldNumber(10, 1, 3600, 1), "duration")
+            .appendField("seconds");
+        this.appendDummyInput().appendField("sample rate")
+            .appendField(new Blockly.FieldDropdown([
+                ["8000 Hz","8000"],["16000 Hz","16000"],["22050 Hz","22050"],
+                ["44100 Hz","44100"],["48000 Hz","48000"]
+            ]), "rate");
+        this.appendDummyInput().appendField("channels")
+            .appendField(new Blockly.FieldDropdown([["1 (mono)","1"],["2 (stereo)","2"]]), "channels");
+        this.setPreviousStatement(true, null); this.setNextStatement(true, null);
         this.setColour(wildlifeColor);
-    },
-};
-
-Blockly.Blocks["dht22_read_safe"] = {
-    init: function() {
-        this.appendDummyInput()
-            .appendField("read and display DHT22 temperature + humidity safely");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(wildlifeColor);
-    },
-};
-
-Blockly.Blocks["photo_filename_timestamp"] = {
-    init: function() {
-        this.appendDummyInput()
-            .appendField("make timestamped photo filename");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(wildlifeColor);
-    },
-};
-
-Blockly.Blocks["camera_take_photo"] = {
-    init: function() {
-        this.appendDummyInput()
-            .appendField("take Raspberry Pi camera photo");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(wildlifeColor);
-    },
-};
-
-Blockly.Blocks["pir_wait_for_no_motion"] = {
-    init: function() {
-        this.appendDummyInput()
-            .appendField("wait until motion stops");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(wildlifeColor);
-    },
+    }
 };
--- generators.ts
+++ generators.ts
@@ -1,63 +1,98 @@
-Blockly.Python["wildlife_imports"] = function() {
-    const code =
-`import time
+Blockly.Python["import_time"] = () => `# Used to pause the program
+import time
+`;
+Blockly.Python["import_subprocess"] = () => `# Used to run Raspberry Pi camera and microphone commands
 import subprocess
+`;
+Blockly.Python["import_datetime"] = () => `# Used to create timestamps for photo and audio filenames
 from datetime import datetime
+`;
+Blockly.Python["import_motion_sensor"] = () => `# Used to control the PIR motion sensor
 from gpiozero import MotionSensor
+`;
+Blockly.Python["import_board"] = () => `# Used to identify Raspberry Pi GPIO pins for the DHT22 sensor
 import board
+`;
+Blockly.Python["import_adafruit_dht"] = () => `# Used to read temperature and humidity from the DHT22 sensor
 import adafruit_dht
 `;
-    return code;
-};
 
 Blockly.Python["pir_setup"] = function(block) {
     const pin = block.getFieldValue("pin");
-    return `pir = MotionSensor(${pin})\n`;
+    return `# Create a PIR motion sensor connected to GPIO ${pin}
+pir = MotionSensor(${pin})
+`;
 };
+Blockly.Python["pir_wait_for_motion"] = () => `# Wait here until the PIR sensor detects motion
+pir.wait_for_motion()
+`;
+Blockly.Python["pir_wait_for_no_motion"] = () => `# Wait here until the PIR sensor no longer detects motion
+pir.wait_for_no_motion()
+`;
 
 Blockly.Python["dht22_setup"] = function(block) {
     const pin = block.getFieldValue("pin");
-    return `dht = adafruit_dht.DHT22(board.D${pin})\n`;
+    return `# Create a DHT22 temperature and humidity sensor connected to GPIO ${pin}
+dht = adafruit_dht.DHT22(board.D${pin})
+`;
 };
-
-Blockly.Python["pir_wait_for_motion"] = function() {
-    return "pir.wait_for_motion()\n";
-};
-
-Blockly.Python["dht22_read_safe"] = function() {
-    const code =
-`try:
+Blockly.Python["dht22_read_safe"] = () => `# Try to read the DHT22 sensor
+try:
+    # Read the temperature in degrees Celsius
     temperature = dht.temperature
+    # Read the relative humidity as a percentage
     humidity = dht.humidity
+    # Display the temperature
     print("Temperature:", temperature, "C")
+    # Display the humidity
     print("Humidity:", humidity, "%")
+# Handle a temporary DHT22 reading error
 except RuntimeError:
+    # Tell the user that the DHT22 sensor could not be read
     print("Could not read the DHT22 sensor.")
 `;
-    return code;
-};
 
-Blockly.Python["photo_filename_timestamp"] = function() {
-    const code =
-`timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+Blockly.Python["camera_take_photo"] = () => `# Get the current date and time and turn it into a timestamp
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+# Use the timestamp to create a unique photo filename
 filename = "/home/pi/photo_" + timestamp + ".jpg"
-`;
-    return code;
-};
-
-Blockly.Python["camera_take_photo"] = function() {
-    const code =
-`subprocess.run([
+# Run the Raspberry Pi camera command to take a picture
+subprocess.run([
     "rpicam-still",
     "-o",
     filename,
     "--nopreview"
 ])
+# Tell the user where the photo was saved
 print("Photo saved:", filename)
 `;
-    return code;
-};
 
-Blockly.Python["pir_wait_for_no_motion"] = function() {
-    return "pir.wait_for_no_motion()\n";
+Blockly.Python["microphone_record"] = function(block) {
+    const device = block.getFieldValue("device");
+    const duration = block.getFieldValue("duration");
+    const rate = block.getFieldValue("rate");
+    const channels = block.getFieldValue("channels");
+    return `# Get the current date and time and turn it into a timestamp
+audio_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+# Use the timestamp to create a unique audio filename
+audio_filename = "/home/pi/audio_" + audio_timestamp + ".wav"
+# Run the Linux audio recording command
+subprocess.run([
+    "arecord",
+    # Select the USB microphone
+    "-D", "${device}",
+    # Record 16-bit audio
+    "-f", "S16_LE",
+    # Set the sample rate
+    "-r", "${rate}",
+    # Set the number of audio channels
+    "-c", "${channels}",
+    # Set how many seconds to record
+    "-d", "${duration}",
+    # Save using the timestamped filename
+    audio_filename
+])
+# Tell the user where the audio recording was saved
+print("Audio saved:", audio_filename)
+`;
 };
