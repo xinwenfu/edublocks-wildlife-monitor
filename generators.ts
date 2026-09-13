4fd33980ab89e01ae30e8417e411585e280ac31d
Xinwen Fu <69218457+xinwenfu@users.noreply.github.com>
2026-09-12 21:30:03 -0400

    Update generators.ts

--- generators.ts
+++ generators.ts
@@ -75,7 +75,7 @@ Blockly.Python["microphone_record"] = function(block) {
     return `# Get the current date and time and turn it into a timestamp
 audio_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 # Use the timestamp to create a unique audio filename
-audio_filename = "pi/audio_" + audio_timestamp + ".wav"
+audio_filename = "audio_" + audio_timestamp + ".wav"
 # Run the Linux audio recording command
 subprocess.run([
     "arecord",
